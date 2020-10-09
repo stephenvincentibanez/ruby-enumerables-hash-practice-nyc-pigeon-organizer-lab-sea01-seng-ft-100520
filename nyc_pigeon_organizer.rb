946c1205e1f25a5bc5d3dd2743f20ec2465e94eb
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-10-09 04:40:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 
 
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_array|
+data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
           if !final_array[name]
